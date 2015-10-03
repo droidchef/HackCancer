7bb1c8303060f8d59bd505bffb4422cd72e21de8
Ishan Khanna <ishan1604@gmail.com>
2015-10-03 15:49:41 -0400

    Too much work in one commit :+1:

--- background.js
+++ background.js
@@ -6,21 +6,33 @@ chrome.tabs.onUpdated.addListener(function(tabId, changedInfo, tab){
   if (changedInfo.status =='complete') {
 
     var tabURL = tab.url;
-    sendURLForCancerCheck(tabURL);
+    sendURLForCancerCheck(tabURL, tab);
     console.log("You are Currently Browsing : " + tabURL);
 
-    chrome.tabs.sendMessage(tab.id, tabURL,
-                                sendURLForCancerCheck);
   }
 
 });
 
-function sendURLForCancerCheck(suspicousTabURL) {
+function sendURLForCancerCheck(suspicousTabURL, tab) {
   if (jQuery) {
     console.log("Jquery is available");
-    var REQUEST_URL = "http://192.168.43.188:4567/" + suspicousTabURL;
+    var REQUEST_URL = "http://10.192.237.191:4567/" + suspicousTabURL;
     $.get(REQUEST_URL, function(data) {
       console.log(data);
+      if (data.response.score == 1) {
+          var opt = {
+            type: "basic",
+            title: "Cancer Hack",
+            message: "This website contains very old and irrellevant content.",
+            iconUrl: "icon.png"
+          };
+        Notification.display(opt);
+      }
+      chrome.tabs.sendMessage(tab.id, suspicousTabURL,
+                                  function(){
+                                    console.log('Message Sent');
+                                  });
+
     })
     .done(function() {
       console.log("DOne");
@@ -33,3 +45,18 @@ function sendURLForCancerCheck(suspicousTabURL) {
     console.log("Jquery is not available");
   }
 }
+
+
+var Notification=(function(){
+    var notification=null;
+
+    return {
+        display:function(opt){
+            notification=chrome.notifications.create(opt);
+            notification.show();
+        },
+        hide:function(){
+            notification.close();
+        }
+    };
+})();
---
+++ options.js
@@ -0,0 +1,33 @@
+// Saves options to chrome.storage
+function save_options() {
+  var shouldKillTabsAuto = document.getElementById('kill').checked;
+  var shoudlOpenYouTube = document.getElementById('youtube').checked;
+  chrome.storage.sync.set({
+    shouldKillTabsAuto: shouldKillTabsAuto,
+    shoudlOpenYouTube: shouldOpenYouTube
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value color = 'red' and shouldKillTabsAuto = true.
+  chrome.storage.sync.get({
+    shouldKillTabsAuto: false,
+    shouldOpenYouTube: false
+  }, function(items) {
+    document.getElementById('kill').checked = items.shouldKillTabsAuto;
+    document.getElementById('youtube').checked = items.shouldOpenYouTube;
+
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);
